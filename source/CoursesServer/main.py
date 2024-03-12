c3a539cc8680aa586ad8c9fcafaca60b83dae0be
Tal Alon <talalon369@gmail.com>
2024-03-12 21:20:47 +0200

    formatted code

--- source/CoursesServer/main.py
+++ source/CoursesServer/main.py
@@ -15,12 +15,11 @@ class Column(StrEnum):
     Lecturer = "שם מרצה"
     StartTime = "שעת התחלה"
     EndTime = "שעת סיום"
-    TotalHours = "סה\"כ שעות"
+    TotalHours = 'סה"כ שעות'
     Classroom = "תיאור כיתה"
     Department = "תיאור חוג"
-    Credits = "נ\"ז"
+    Credits = 'נ"ז'
     CourseType = "סוג מקצוע"
-
 
 
 class Day(Enum):
@@ -43,13 +42,29 @@ class Course:
 
     def add_lecture(self, row) -> None:
         self.lectures.append(
-            Lesson(row[Column.Group], row[Column.Subject], row[Column.Day], row[Column.Lecturer], row[Column.StartTime],
-                   row[Column.EndTime], row[Column.Classroom]))
+            Lesson(
+                row[Column.Group],
+                row[Column.Subject],
+                row[Column.Day],
+                row[Column.Lecturer],
+                row[Column.StartTime],
+                row[Column.EndTime],
+                row[Column.Classroom],
+            )
+        )
 
     def add_exercise(self, row) -> None:
         self.exercises.append(
-            Lesson(row[Column.Group], row[Column.Subject], row[Column.Day], row[Column.Lecturer], row[Column.StartTime],
-                   row[Column.EndTime], row[Column.Classroom]))
+            Lesson(
+                row[Column.Group],
+                row[Column.Subject],
+                row[Column.Day],
+                row[Column.Lecturer],
+                row[Column.StartTime],
+                row[Column.EndTime],
+                row[Column.Classroom],
+            )
+        )
 
 
 @dataclass
@@ -64,10 +79,12 @@ class Lesson:
 
     @property
     def duration(self) -> timedelta:
-        return datetime.combine(date.min, self.end_time) - datetime.combine(date.min, self.start_time)
+        return datetime.combine(date.min, self.end_time) - datetime.combine(
+            date.min, self.start_time
+        )
 
 
-def read_courses_file() -> pd.DataFrame: 
+def read_courses_file() -> pd.DataFrame:
     courses_table_datapath = "./input/coursesFile.xlsx"
     courses_data = pd.read_excel(courses_table_datapath)
     return courses_data
@@ -81,19 +98,19 @@ def init_courses_map() -> dict[str, Course]:
     # Iterate through the DataFrame
     for index, row in courses_data.iterrows():
         # Get the current value
-        curr_course = row['תיאור נושא'] # שם הקורס
+        curr_course = row["תיאור נושא"]  # שם הקורס
 
         # Check if the course is already in the map
         if curr_course not in courses_map:
             # If not, create a new Course object for this course
             courses_map[curr_course] = Course(
-                department=row['תיאור חוג'],
-                subject=row['תיאור נושא'], # שם הקורס
-                credits=row['נ"ז']
+                department=row["תיאור חוג"],
+                subject=row["תיאור נושא"],  # שם הקורס
+                credits=row['נ"ז'],
             )
 
         # If the course type is not None and is a lecture or exercise, add it to the respective list
-        if row['סוג מקצוע'] not in [7,13,14,15]:
+        if row["סוג מקצוע"] not in [7, 13, 14, 15]:
             courses_map[curr_course].add_lecture(row)
         else:
             courses_map[curr_course].add_exercise(row)
@@ -103,9 +120,11 @@ def init_courses_map() -> dict[str, Course]:
 
 def main():
     courses = init_courses_map()
-    with open('output.txt', 'w') as file:
+    with open("output.txt", "w") as file:
         for course_name, course in courses.items():
-            file.write(f"--------------------------------Course: {course_name} ------------------------------------\n")
+            file.write(
+                f"--------------------------------Course: {course_name} ------------------------------------\n"
+            )
             file.write(f"Department: {course.department}\n")
             file.write(f"Credits: {course.credits}\n")
             file.write("Lectures:\n")
@@ -119,7 +138,9 @@ def main():
                 file.write(f"Duration: {lecture.duration}\n")
                 file.write(f"Classroom: {lecture.classroom}\n")
                 file.write("\n")
-            file.write("--------------------------------Exercises:------------------------------------\n")
+            file.write(
+                "--------------------------------Exercises:------------------------------------\n"
+            )
             for exercise in course.exercises:
                 file.write(f"Group: {exercise.group}\n")
                 file.write(f"Subject: {exercise.subject}\n")
@@ -131,6 +152,7 @@ def main():
                 file.write(f"Classroom: {exercise.classroom}\n")
                 file.write("\n")
             file.write("\n")
+
 
 if __name__ == "__main__":
     main()
