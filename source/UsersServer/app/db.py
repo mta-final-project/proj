ee8f59a8ce5c0fcd66a243de9d20a538f96a1e36
Omer Migdal <omermigdali@gmail.com>
2024-03-25 18:29:49 +0200

    added service for validating email domain

---
+++ source/UsersServer/app/db.py
@@ -0,0 +1,17 @@
+
+
+import motor.motor_asyncio
+from beanie import Document
+from fastapi_users.db import BeanieBaseUser
+from fastapi_users_db_beanie import BeanieUserDatabase
+
+DATABASE_URL = "mongodb://user:pass@localhost:27017"
+client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL, uuidRepresentation="standard")
+db = client["courses"]
+
+
+class User(BeanieBaseUser, Document):
+   pass
+
+async def get_user_db():
+    yield BeanieUserDatabase(User)
