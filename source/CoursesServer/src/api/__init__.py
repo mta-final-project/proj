7182796fe1446011abe4a54d171f849e31fda723
Tal Alon <talalon369@gmail.com>
2024-03-26 22:54:23 +0200

    include schedule api in the app

--- source/CoursesServer/src/api/__init__.py
+++ source/CoursesServer/src/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from src.api.courses.endpoints import router as courses_router
+from src.api.schedule.endpoints import router as schedule_router
 
 router = APIRouter()
 router.include_router(courses_router)
+router.include_router(schedule_router)
