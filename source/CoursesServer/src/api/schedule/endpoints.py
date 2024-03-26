566c0598265bae0a4729ead76b9289935cbe6611
Tal Alon <talalon369@gmail.com>
2024-03-26 22:53:57 +0200

    update api

--- source/CoursesServer/src/api/schedule/endpoints.py
+++ source/CoursesServer/src/api/schedule/endpoints.py
@@ -1,14 +1,28 @@
-from typing import Annotated
+from fastapi import APIRouter, status, Query
+from beanie import PydanticObjectId
 
-from fastapi import APIRouter, status, Depends
-
-from src.api.schedule.schemas import GetCombiSchema as _GetCombiSchema
+from src.api.schedule.schemas import SelectedGroupsSchema, GroupViewSchema
+from src.apps.courses.models import Course
+from src.apps.shcedule import service
 
 router = APIRouter(prefix="/schedule")
 
-GetCombiSchema = Annotated[_GetCombiSchema, Depends(_GetCombiSchema)]
-
 
 @router.get("/combinations", status_code=status.HTTP_200_OK)
-async def get_combinations(params: GetCombiSchema):
-    ...
+async def get_combinations(
+        courses_ids: list[PydanticObjectId] = Query(default=None)
+) -> list[SelectedGroupsSchema]:
+
+    courses_ids = courses_ids or []
+    courses = [await Course.get(_id) for _id in courses_ids]
+
+    options = service.get_optional_combinations(courses)
+    result = []
+    for option in options:
+        groups = [
+            GroupViewSchema(**dict(group), course_id=course_id)
+            for group, course_id in zip(option, courses_ids)
+        ]
+        result.append(SelectedGroupsSchema(groups=groups))
+
+    return result
--- source/CoursesServer/src/api/schedule/schemas.py
+++ source/CoursesServer/src/api/schedule/schemas.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
 from beanie import PydanticObjectId
+from pydantic import BaseModel
 
 
-class GetCombiSchema(BaseModel):
-    selected_courses: list[PydanticObjectId]
+class GroupViewSchema(BaseModel):
+    course_id: PydanticObjectId
+    group: int
+    description: str
+    lecturer: str
+
+
+class SelectedGroupsSchema(BaseModel):
+    groups: list[GroupViewSchema]
