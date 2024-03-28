054b18bce6856c91a38860811fd3af0c880b547b
Tal Alon <talalon369@gmail.com>
2024-03-28 23:26:34 +0200

    added a router

--- source/ui/src/App.js
+++ source/ui/src/App.js
@@ -1,9 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppBar from "./layout/AppBar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>ACADEM-EASE</h1>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <AppBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
